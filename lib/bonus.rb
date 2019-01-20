dd9187290abee53a4a24d39042a79ba9952b16d1
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-20 04:26:28 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -8,13 +8,13 @@ puts epic_tragedy = {
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"}, 
+          {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
@@ -22,7 +22,7 @@ puts epic_tragedy = {
 
   #code your solution here:
 
-  
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
